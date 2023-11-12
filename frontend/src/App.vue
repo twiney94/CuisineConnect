3ee7c10e719d2038017bace921f2daaf8d8543e5
Yassine Bekioui <mp482a@hotmail.fr>
2023-11-12 14:55:24 +0100

    init project frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { FwbButton, FwbNavbar, FwbNavbarCollapse, FwbNavbarLink, FwbNavbarLogo } from 'flowbite-vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <fwb-navbar>
+        <template #logo>
+          <fwb-navbar-logo alt="Flowbite logo" image-url="/images/logo.svg" link="#">
+            Flowbite
+          </fwb-navbar-logo>
+        </template>
+        <template #default="{ isShowMenu }">
+          <fwb-navbar-collapse :is-show-menu="isShowMenu">
+            <fwb-navbar-link is-active link="#">
+              <RouterLink to="/"> Home </RouterLink>
+            </fwb-navbar-link>
+            <fwb-navbar-link link="#">
+              <RouterLink to="/services"> Services </RouterLink>
+            </fwb-navbar-link>
+            <fwb-navbar-link link="#"> Pricing </fwb-navbar-link>
+            <fwb-navbar-link link="#"> Contact </fwb-navbar-link>
+          </fwb-navbar-collapse>
+        </template>
+        <template #right-side>
+          <fwb-button> Get started </fwb-button>
+        </template>
+      </fwb-navbar>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,vue}',
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('flowbite/plugin')]
+}

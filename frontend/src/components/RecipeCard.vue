2b3c8be64580d89005cb6004a143a6ec2d3e3164
Yass <61833982+twiney94@users.noreply.github.com>
2024-01-10 09:49:02 +0100

    corrected build error

--- frontend/src/components/RecipeCard.vue
+++ frontend/src/components/RecipeCard.vue
@@ -2,7 +2,7 @@
   <a href="#" class="block max-w-sm dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <fwb-card
       v-if="!compact"
-      @click="goToRecipe(recipeProp)"
+      @click="goToRecipe(recipeProp as Recipe)"
       img-alt="Desk"
       img-src="https://www.foodandwine.com/thmb/tAS-x_IC4ss1cb9EdDpsr0UExdM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/bucatini-with-mushroom-ragu-dandelion-greens-and-tarragon-FT-RECIPE0421-3a5f0d29f7264f5e9952d4a3a51f5f58.jpg"
       variant="image"

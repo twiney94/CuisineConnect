55c72c63c892cc9aab55000577f47835d72e7034
Yass <61833982+twiney94@users.noreply.github.com>
2024-01-10 10:59:31 +0100

    port add

--- AI/src/server.js
+++ AI/src/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const app = require('./app')
 
+const PORT = process.env.PORT || 3000;
 
-const server = app.listen(3000, () => {
+const server = app.listen(PORT, () => {
     console.log('Server listening on port 3000');
 });
 
